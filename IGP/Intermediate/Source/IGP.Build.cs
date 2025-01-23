0cdb3ca9aff5cbb9ad5f2041c246947afe61de17
Matitrax <matimilisosa@gmail.com>
2025-01-23 19:10:47 -0300

    .

---
+++ IGP/Intermediate/Source/IGP.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class IGP : ModuleRules
+{
+	public IGP(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
